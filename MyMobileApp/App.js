ea2e54e1effa3aad08ccbb0361007c1ef0fe1c58
madurangaPrabhath <maduracourse@gmail.com>
2024-12-25 13:42:01 +0530

    Create Home component

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Home from './components/Home'
 
 export default function App() {
   return (
     <PaperProvider>
       <SafeAreaView>
-        <KeyboardAvoidingView>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
           <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
